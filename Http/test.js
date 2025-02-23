a0c07c31b0496213a525835e2249fbb14c017485
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-02-23 17:12:49 +0600

    .

---
+++ Http/test.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('Method:', req.method);
+  console.log('URL:', req.url);
+  console.log('Headers:', req.headers.host);
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Request received');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
